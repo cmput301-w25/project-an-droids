891ca5df9cf4bdcbf5ac71d49559aca66a5f7efe
dbatra11 <dbatra@ualberta.ca>
2025-03-31 12:10:41 -0600

    Android Tests Fixed and Started

--- code/app/src/androidTest/java/com/example/an_droids/MoodAppTest.java
+++ code/app/src/androidTest/java/com/example/an_droids/MoodAppTest.java
@@ -1,104 +1,65 @@
 package com.example.an_droids;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Build;
-import androidx.annotation.NonNull;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.IdlingResource;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+
 import com.google.firebase.firestore.FirebaseFirestore;
-import org.junit.After;
-import org.junit.Before;
+
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
-import static org.hamcrest.Matchers.anything;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import java.util.Date;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MoodAppTest {
 
-//    @Rule
-//    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
-//
-//    private MoodProvider moodProvider;
-//
-//    @Before
-//    public void setUp() {
-//        moodProvider = new MoodProvider();
-//    }
-//
-//    @After
-//    public void tearDown() {
-//        FirebaseFirestore.getInstance().collection("Moods").get().addOnCompleteListener(task -> {
-//            if (task.isSuccessful() && task.getResult() != null) {
-//                task.getResult().forEach(document -> document.getReference().delete());
-//            }
-//        });
-//    }
+    @Rule
+    public ActivityScenarioRule<LoginActivity> activityRule =
+            new ActivityScenarioRule<>(LoginActivity.class);
 
+    @BeforeClass
+    public static void setUp() {
+        String androidLocalhost = "10.0.2.2";
+        int portNumber = 8080;
+        FirebaseFirestore.getInstance().useEmulator(androidLocalhost, portNumber);
+    }
+    @Test
+    public void testLogin() {
+        onView(withId(R.id.emailInput)).check(matches(isDisplayed()));
+        onView(withId(R.id.passwordInput)).check(matches(isDisplayed()));
+        onView(withId(R.id.loginButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.emailInput))
+                .perform(typeText("test@example.com"), closeSoftKeyboard());
 
-//    @Test
-//    public void testEditMood() {
-//        Mood mood = new Mood("Happiness", "Feeling good", null, new Date(), null, "Alone", Mood.Privacy.PUBLIC);
-//        moodProvider.addMood(mood, new MoodProvider.OnMoodOperationListener() {
-//            @Override
-//            public void onSuccess() {
-//                activityRule.getScenario().onActivity(activity -> {
-//                    onView(withText("Happiness 😃")).perform(click());
-//                    onView(withId(R.id.reasonEditText)).check(matches(isDisplayed()));
-//                    onView(withId(R.id.reasonEditText)).perform(replaceText("Feeling great"));
-//                    onView(withText("Save")).perform(click());
-//
-//                    onView(withId(R.id.moodTitle)).check(matches(withText("Feeling great")));
-//                });
-//            }
-//
-//            @Override
-//            public void onFailure(Exception e) {
-//            }
-//        });
-//    }
-//
-//    @Test
-//    public void testDeleteMood() {
-//        Mood mood = new Mood("Happiness", "Feeling good", null, new Date(), null, "Alone", Mood.Privacy.PRIVATE);
-//        moodProvider.addMood(mood, new MoodProvider.OnMoodOperationListener() {
-//            @Override
-//            public void onSuccess() {
-//                activityRule.getScenario().onActivity(activity -> {
-//                    onView(withText("Happiness 😃")).perform(ViewActions.longClick());
-//                    onView(withText("Yes")).perform(click());
-//
-//                    onView(withId(R.id.moodList)).check(matches(withText("")));
-//                });
-//            }
-//
-//            @Override
-//            public void onFailure(Exception e) {
-//            }
-//        });
-//    }
+        // Click login button
+        onView(withId(R.id.loginButton)).perform(click());
+        onView(withId(R.id.login_container)).check(matches(isDisplayed()));
+        onView(withId(R.id.signupLink)).perform(click());
+
+        // Verify we moved to register activity
+        // Replace RegisterActivity.class with your actual register activity
+        ActivityScenario<SignupActivity> scenario = ActivityScenario.launch(SignupActivity.class);
+        onView(withId(R.id.signup_container)).check(matches(isDisplayed()));
+        onView(withId(R.id.loginLink)).perform(click());
+        onView(withId(R.id.login_container)).check(matches(isDisplayed()));
+        onView(withId(R.id.emailInput))
+                .perform(typeText("swilson@ualberta.ca"), closeSoftKeyboard());
+        onView(withId(R.id.passwordInput))
+                .perform(typeText("Test@1234"), closeSoftKeyboard());
+        onView(withId(R.id.loginButton)).perform(click());
+        ActivityScenario<MainActivity> main = ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.main_container)).check(matches(isDisplayed()));
+    }
 }
-
-
-
